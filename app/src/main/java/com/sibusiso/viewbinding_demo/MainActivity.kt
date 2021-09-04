899a4b9705c92009ac477e24f1cc25425cb8a049
Kingsbu <sibusisok32@gmail.com>
2021-09-04 09:39:06 +0200

    initial working project commit

--- app/src/main/java/com/sibusiso/viewbinding_demo/MainActivity.kt
+++ app/src/main/java/com/sibusiso/viewbinding_demo/MainActivity.kt
@@ -2,10 +2,25 @@ package com.sibusiso.viewbinding_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button_submit)
+
+        button.setOnClickListener{
+            displayGreeting()
+        }
+    }
+
+    private fun displayGreeting() {
+        val messageView = findViewById<TextView>(R.id.textView_show_input)
+        val nameText = findViewById<EditText>(R.id.editText_name)
+        messageView.text = "Hello " + nameText.text
     }
 }

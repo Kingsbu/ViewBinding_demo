8cf992fdc9e3f9e483b357dc28bef4d3058af6f3
Kingsbu <sibusisok32@gmail.com>
2021-09-04 09:53:05 +0200

    implement binding class on MainActivity.kt

--- app/src/main/java/com/sibusiso/viewbinding_demo/MainActivity.kt
+++ app/src/main/java/com/sibusiso/viewbinding_demo/MainActivity.kt
@@ -5,22 +5,30 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.sibusiso.viewbinding_demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val button = findViewById<Button>(R.id.button_submit)
-
-        button.setOnClickListener{
+        //val button = findViewById<Button>(R.id.button_submit)
+        //button.setOnClickListener{
+        //    displayGreeting()
+        //}
+        
+        binding.buttonSubmit.setOnClickListener{
             displayGreeting()
         }
     }
 
     private fun displayGreeting() {
-        val messageView = findViewById<TextView>(R.id.textView_show_input)
-        val nameText = findViewById<EditText>(R.id.editText_name)
-        messageView.text = "Hello " + nameText.text
+        //val messageView = findViewById<TextView>(R.id.textView_show_input)
+        //val nameText = findViewById<EditText>(R.id.editText_name)
+        //messageView.text = "Hello " + nameText.text
+        binding.textViewShowInput.text = binding.editTextName.text
     }
 }
